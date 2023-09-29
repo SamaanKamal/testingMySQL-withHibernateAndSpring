e480ade983adcb1269309cb960446503a1b1b574
Samaan Kamal <samaankamal72@gmail.com>
2023-09-29 23:50:40 +0300

    now you can switch between lazy and eager fetch types in instructor class

---
+++ src/main/java/EagerAndLazyLoading/CreateInstructor.java
@@ -0,0 +1,64 @@
+package EagerAndLazyLoading;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructor {
+    public static void main(String[] args) {
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/hb-03-one-to-many")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "samaan11")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("hibernate.show_sql", "true") // Enable SQL logging for debugging
+                .setProperty("current.session.context.class","thread")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.openSession();
+
+        try {
+            // create the objects
+            Instructor tempInstructor =
+                    new Instructor("Susan", "Public", "susan.public@luv2code.com");
+
+            InstructorDetail tempInstructorDetail =
+                    new InstructorDetail(
+                            "http://www.youtube.com",
+                            "Video Games");
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            //
+            // Note: this will ALSO save the details object
+            // because of CascadeType.ALL
+            //
+            System.out.println("Saving instructor: " + tempInstructor);
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            // handle connection leak issue
+            session.close();
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/EagerAndLazyLoading/MainInstructor.java
@@ -0,0 +1,58 @@
+package EagerAndLazyLoading;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class MainInstructor {
+    public static void main(String[] args) {
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/hb-03-one-to-many")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "samaan11")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("hibernate.show_sql", "true") // Enable SQL logging for debugging
+                .setProperty("current.session.context.class","thread")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.openSession();
+
+        try {
+            // Start a transaction
+            session.beginTransaction();
+            // get the instructor detail object
+            int theId = 1;
+            Instructor tempInstructor =
+                    session.get(Instructor.class, theId);
+
+            // create some courses
+            Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+            Course tempCourse2 = new Course("The Pinball Masterclass");
+
+            // add courses to instructor
+            tempInstructor.add(tempCourse1);
+            tempInstructor.add(tempCourse2);
+
+            // Commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            // handle connection leak issue
+            session.close();
+            factory.close();
+        }
+    }
+}
