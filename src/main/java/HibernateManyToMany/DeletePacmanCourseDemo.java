bc0bb98a5d207ad095c611d46fb29031bb87efd1
Samaan Kamal <samaankamal72@gmail.com>
2023-10-10 19:09:58 +0300

    verifying Relationship by deleting PacmanCourse for all students

---
+++ src/main/java/HibernateManyToMany/DeletePacmanCourseDemo.java
@@ -0,0 +1,56 @@
+package HibernateManyToMany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeletePacmanCourseDemo {
+    public static void main(String[] args) {
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/hb-05-many-to-many")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "samaan11")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("hibernate.show_sql", "true") // Enable SQL logging for debugging
+                .setProperty("current.session.context.class","thread")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.openSession();
+
+        try {
+            // Start a transaction
+            session.beginTransaction();
+
+            // get the pacman course from db
+            int courseId = 10;
+            Course tempCourse = session.get(Course.class, courseId);
+
+            // delete the course
+            System.out.println("Deleting course: " + tempCourse);
+
+            session.delete(tempCourse);
+
+            // Commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            // handle connection leak issue
+            session.close();
+            factory.close();
+        }
+    }
+}
